e3eada248debd2b8d5283914450f92b8666422a6
Ray <rayzlui@gmail.com>
2021-05-07 10:01:53 -0700

    remove store call from component

--- src/views/buttons/GameModeSelectView.js
+++ src/views/buttons/GameModeSelectView.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { CLASSIC_MODE, TIMED_MODE } from "../../actions/actionTypes";
-import store from "../../createStore";
+import { PropTypes } from "prop-types";
 
-export function GameModeSelectView() {
+export function GameModeSelectView(props) {
+  let { selectClassic, selectTimed } = props;
   return (
     <section className={"intro_screen"}>
-      <button
-        onClick={() => store.dispatch({ type: CLASSIC_MODE })}
-        className={"game_options"}
-      >
+      <button onClick={() => selectClassic()} className={"game_options"}>
         Classic Mode
       </button>
-      <button
-        onClick={() => store.dispatch({ type: TIMED_MODE })}
-        className={"game_options"}
-      >
+      <button onClick={() => selectTimed()} className={"game_options"}>
         Timed Mode
       </button>
     </section>
   );
 }
+
+GameModeSelectView.propTypes = {
+  selectClassic: PropTypes.func,
+  selectTimed: PropTypes.func,
+};
