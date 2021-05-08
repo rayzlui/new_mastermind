bcdda427b7232b4c9d23c8280d1594a58ad0b49a
Ray <rayzlui@gmail.com>
2021-05-07 18:11:28 -0700

    fix bug where new chidden code was not generated for second player in two player mode

--- src/containers/CountdownTimerContainer.js
+++ src/containers/CountdownTimerContainer.js
@@ -1,27 +1,69 @@
 import { connect } from "react-redux";
-import { changeTurn, gameWon } from "../actions/actions";
+import { changeTurn, gameWon, versusComputer } from "../actions/actions";
 import { SET_SCREEN_CHANGE } from "../actions/actionTypes";
 import { CountdownTimer } from "../views/CountdownTimer";
 
 function mapStateToProps(state) {
   return {
-    timeAllowed: state.advancedOptions.timeAllowed,
+    timeAllowed: state.advancedOptions?.timeAllowed,
     isTwoPlayer: state.isTwoPlayer,
     turnChange: state.turnChange,
+    advancedOptions: state.advancedOptions,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    endGame: () => dispatch(gameWon()),
-    changeTurn: () => {
+    finishGame: () => dispatch(gameWon()),
+    changePlayers: (
+      codeLength,
+      codeOptions,
+      turnsAllowed,
+      timeAllowed,
+      turnsMade
+    ) => {
       dispatch({ type: SET_SCREEN_CHANGE });
       dispatch(changeTurn());
+      dispatch(
+        versusComputer([
+          codeLength,
+          codeOptions,
+          turnsAllowed,
+          timeAllowed,
+          turnsMade,
+        ])
+      );
     },
+  };
+}
+
+function mergeProps(mapStateToProps, mapDispatchToProps) {
+  let {
+    timeAllowed,
+    isTwoPlayer,
+    turnChange,
+    advancedOptions,
+  } = mapStateToProps;
+  let { finishGame, changePlayers } = mapDispatchToProps;
+  let { codeLength, codeOptions, turnsAllowed, turnsMade } = advancedOptions;
+  return {
+    timeAllowed,
+    isTwoPlayer,
+    turnChange,
+    endGame: () => finishGame(),
+    changeTurn: () =>
+      changePlayers(
+        codeLength,
+        codeOptions,
+        turnsAllowed,
+        timeAllowed,
+        turnsMade
+      ),
   };
 }
 
 export const CountdownTimerContainer = connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
+  mergeProps
 )(CountdownTimer);
